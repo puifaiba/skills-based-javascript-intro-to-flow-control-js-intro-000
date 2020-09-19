d377aaf806a0c8b8e8e90d6c6bd6160b0f0063f7
Boonie Attawut <puifai.ba@gmail.com>
2020-09-19 19:06:25 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,10 +25,21 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
 switch(age) {
-    case age > 12 && age < 20:
-        return("You are a teenager");
-        break;
+    case 13:
+        return "You are a teenager";
+    case 14:
+        return "You are a teenager";
+    case 15:
+        return "You are a teenager";
+    case 16:
+        return "You are a teenager";
+    case 17:
+        return "You are a teenager";
+    case 18:
+        return "You are a teenager";
+    case 19:
+        return "You are a teenager";
     default:
-        return("You have an age");
+        return "You have an age";
 }
 }
